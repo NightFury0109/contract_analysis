354d965efcdec3ec1031bfe68272ad702f9a678f
NightFury0109 <nightfury0109@gmail.com>
2022-08-30 18:52:32 +0200

    [complete]: Header

---
+++ src/components/header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <>
+      <div className="flex flex-row items-center justify-between">
+        <div className="flex items-center cursor-pointer">
+          <img src="assets/images/logo.png" alt="logo" width='45px' height='45px' />
+          <span className='ml-2 text-3xl font-bold'>SHIELD</span>
+        </div>
+
+        <div className="flex items-center cursor-pointer">
+          <span className='text-xl font-bold mr-9'>Get in Touch</span>
+          <img src="assets/images/icons/arrow_right.png" alt="arrow_right" height='20px' />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Header;
